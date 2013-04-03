1ab29227ed77a3f8af91c2ed1ea3479d59b1b600
Peter Kordel <pkordel@gmail.com>
2013-04-03 19:56:19 +0700

    Uses cache store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
-
-Blogs::Application.config.session_store :cookie_store, key: '_blogs_session', domain: 'blogs.dev'
+  
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.week, domain: 'blogs.dev'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
